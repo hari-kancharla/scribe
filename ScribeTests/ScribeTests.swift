328ebce7bdef6b120f37d747cef7917d4380ec42
hk <162476937+hari-kancharla@users.noreply.github.com>
2026-08-27 12:00:33 +0530

    Add unit tests

---
+++ ScribeTests/ScribeTests.swift
@@ -0,0 +1,15 @@
+//
+//  ScribeTests.swift
+//  ScribeTests
+//
+
+import Testing
+@testable import Scribe
+
+struct ScribeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
